e5d236daee57d1bb8879b85147c02984a524058d
Mohamed Matar <matar.mohamed@gmx.de>
2026-09-23 22:11:38 +0200

    Base Avalonia App

---
+++ Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+using CommunityToolkit.Mvvm.Input;
+
+namespace TimeViewer.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
